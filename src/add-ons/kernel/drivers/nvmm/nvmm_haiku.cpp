8baee35f2fc20964502238119b50c8d91e4e32eb
Daniel Martin <dalmemail@gmail.com>
2024-05-23 14:02:18 +0200

    Fix os_pagemem_zalloc()

--- src/add-ons/kernel/drivers/nvmm/nvmm_haiku.cpp
+++ src/add-ons/kernel/drivers/nvmm/nvmm_haiku.cpp
@@ -74,9 +74,8 @@ os_pagemem_zalloc(size_t size)
 	area_id area = create_area(NULL, &ptr, B_ANY_KERNEL_ADDRESS,
 		alloc_size, B_FULL_LOCK, B_READ_AREA | B_WRITE_AREA);
 
-	if (area < 0) {
-		panic("%s: %s", __func__, strerror(-area));
-	}
+	if (area < 0)
+		return NULL;
 
 	memset(ptr, 0, alloc_size);
 
